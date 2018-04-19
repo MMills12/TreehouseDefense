595809f8fb854754e0589d24d0a6b71553a5c077
Michael Mills <michaelmills@quickenloans.com>
2018-04-19 11:26:40 -0400

    Initial commit

---
+++ TreehouseDefense/Level.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Level
+    {
+        private readonly IInvader[] _invaders;
+        public Tower[] Towers { get; set; }
+        public Level(IInvader[] invaders)
+        {
+            _invaders = invaders;
+        }
+        //Returns: true if the player wins, false otherwise.
+        public bool Play()
+        {
+            //Run until all invaders are neutralized or invader reaches end of path.
+            int remainingInvaders = _invaders.Length;
+            while(remainingInvaders > 0)
+            {
+                //Each tower has opportunity to fire on invaders.
+                foreach(Tower tower in Towers)
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
+                //Count and move the invaders that are still active.
+                remainingInvaders = 0;
+                foreach(IInvader invader in _invaders)
+                {
+                    if (invader.IsActive)
+                    {
+                        invader.Move();
+                        if (invader.HasScored)
+                        {
+                            return false;
+                        }
+                        remainingInvaders++;
+                    }
+                }
+
+            }
+            return true;
+
+        }
+
+    }
+}
---
+++ TreehouseDefense/Tower.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Tower
+    {
+        protected virtual int Range { get; } = 1;
+        protected virtual int Power { get; } = 1;
+        protected virtual double Accuracy { get; } = .75;
+
+        private readonly MapLocation _location;
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+        public bool IsSuccessfulShot()
+        {
+            return Random.NextDouble() < Accuracy;
+        }
+        public void FireOnInvaders(IInvader[] invaders)
+        {
+            //int index = 0;
+            ////While Loop
+            //while (index < invaders.Length)
+            //{
+            //    Invader invader = invaders[index];
+            //    //Do stuff with invader
+
+            //    index++;               
+            //}
+            ////For Loop
+            //for(int i = 0; i < invaders.Length; i++)
+            //{
+            //    Invader invader = invaders[i];
+            //    //Do stuff with invader
+            //}
+            
+            foreach(IInvader invader in invaders)
+            {
+                //Do stuff with invader
+                if(invader.IsActive && _location.InRangeOf(invader.Location, Range))
+                {
+                    if (IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(Power);
+                        
+
+                        if (invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Neutralized an invader at " + invader.Location + "!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot at and missed an invader.");
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
